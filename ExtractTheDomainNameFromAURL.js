fcad5ea670996b39017d5df093eb9f7078fd5b67
Mikey <mikeywearing@gmail.com>
2021-06-30 16:29:00 +0200

    ExtractTheDomainNameFromAURL

---
+++ ExtractTheDomainNameFromAURL.js
@@ -0,0 +1,12 @@
+const domainName = (url) => {
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+  return url.split(".")[0];
+};
+
+console.log(" ");
+console.log(domainName("http://google.com"), "=== google");
+console.log(domainName("http://google.co.jp"), "=== google");
+/* console.log(domainName("www.xakep.ru"), "xakep"); */
+console.log(domainName("https://youtube.com"), "=== youtube");
